5efc14dd0bfa6d9e51e8834c1a22bd42bbc74b75
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-01-09 12:24:40 +0000

    Added v2 of the simulation

--- main.c
+++ main.c
@@ -1,109 +1,243 @@
-#include "raylib.h"
 #include <stdio.h>
+#include "raylib.h"
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
+#define RAYLIB_NUKLEAR_IMPLEMENTATION
+#define RAYLIB_NUKLEAR_INCLUDE_DEFAULT_FONT
+#include "raylib-nuklear.h"
 #include <stdbool.h>
 
+typedef enum { UP, RIGHT, DOWN, LEFT } Direction;
+typedef enum { R, L, U, C } Rule;
 
-typedef enum{
-    UP,
-    RIGHT,
-    DOWN,
-    LEFT
-} direction;
-
-typedef struct{
-    int x;
-    int y;
-    direction dir;
+typedef struct {
+    char cAntX[8];
+    char cAntY[8];
+    int antX;
+    int antY;
+    Direction antDir;
+    bool antDead;
 } Ant;
 
-Ant Ants[8];
+typedef struct {
+    int r;
+    int g;
+    int b;
+    char cr[8];
+    char cg[8];
+    char cb[8];
+    Rule colourRule;
+} tileColour;
 
-void defaultAnt(){ //starts a 200x200 grid sized up to 5px in a 1000x1000 display
-    //char *ask;
-    //printf("How many ants do you want?");
-    //scanf("%s",ask);
+Ant ants[8];
+tileColour Colours[20];
+static int grid[200][200];
+int colourCount = 2;
+int antcount = 1;
+int antlen[8][3] = {0};
+int colourlen[20][3] = {0};
+int selected_dirs[8] = {0};
+int selected_rules[20] = {0};
+char dirs[4][7] = {"UP", "RIGHT", "DOWN", "LEFT"};
+const char *dir_pointers[] = { dirs[0], dirs[1], dirs[2], dirs[3] };
+char rules[4][4] = {"R", "L", "U", "C"};
+const char *rule_pointers[] = { rules[0], rules[1], rules[2], rules[3] };
 
-    Ants[0].x = 50;
-    Ants[0].y = 50;
-    Ants[0].dir = DOWN;
+void InitColourStruct() {
+    for (int i = 0; i < 20; i++) {
+        Colours[i].r = rand() % 255;
+        Colours[i].g = rand() % 255;
+        Colours[i].b = rand() % 255;
+        Colours[i].colourRule = rand() % 4;
+        char bufferR[8];
+        char bufferG[8];
+        char bufferB[8];
+        snprintf(Colours[i].cr, sizeof Colours[i].cr, "%d", Colours[i].r);
+        snprintf(Colours[i].cg, sizeof Colours[i].cg, "%d", Colours[i].g);
+        snprintf(Colours[i].cb, sizeof Colours[i].cb, "%d", Colours[i].b);
 
-    int grid[200][200];
-    for(int i = 0; i < 200; i++){
-        for(int j = 0; j < 200; j++){
-            grid[i][j] = 0;
+        for (int j = 0; j < 3; j++) {
+            colourlen[i][j] = 0;
         }
     }
-
-    bool AntDead = false;
-    InitWindow(1000,1000,"simulation");
-    RenderTexture2D RenderGrid = LoadRenderTexture(1000,1000);
-    SetTargetFPS(10000);
-    ClearBackground(RAYWHITE);
-    double iteration = 0;
-    while (!AntDead && !WindowShouldClose()) {
-        
-        int x = Ants[0].x;
-        int y = Ants[0].y;
-
-        if (x < 0 || x >= 200 || y < 0 || y >= 200) {
-            AntDead = true;
-            break;
-        }
-
-        
-        BeginTextureMode(RenderGrid);
-
-        if (grid[y][x] == 0) {
-
-            grid[y][x] = 1;
-            DrawRectangle(x * 5, y * 5, 5, 5, BLACK);
-            Ants[0].dir = (Ants[0].dir + 1) % 4;
-        } else {
-  
-            grid[y][x] = 0;
-            DrawRectangle(x * 5, y * 5, 5, 5, RAYWHITE);
-            Ants[0].dir = (Ants[0].dir + 3) % 4; 
-        }
-
-        switch (Ants[0].dir) {
-            case UP:    Ants[0].y--; break;
-            case RIGHT: Ants[0].x++; break;
-            case DOWN:  Ants[0].y++; break;
-            case LEFT:  Ants[0].x--; break;
-        }
-        EndTextureMode();
-
-        if (Ants[0].x < 0 || Ants[0].x >= 200 ||
-            Ants[0].y < 0 || Ants[0].y >= 200) {
-            AntDead = true;
-            break;
-        }
-        iteration++;
-         BeginDrawing();
-         ClearBackground(RAYWHITE);
-         DrawTexture(RenderGrid.texture, 0, 0, RAYWHITE);
-         EndDrawing();
-    }
-
-
 }
-
-void CLI(){
-    while(1){
-        char ask[20];
-        printf("Type 'default' for the classic simulation \nType 'random' for randomly generated simulation with different rules\nType 'custom' for setting your own rules\nType 'symmetry' for some preset symmetrical grids");
-        scanf("%s",ask);
-        if(strcmp(ask,"default") == 0) defaultAnt(); //ask how many ants
-        //else if(strcmp(ask,"random") == 0) randomAnt();
-        //else if(strcmp(ask,"custom") == 0) customAnt();
-        //else if(strcmp(ask,"symmetry") == 0) symmetricalAnt; 
-        else printf("invalid choice, try again");
+void InitAntStruct(){
+    for(int i = 0; i < 8; i++){
+        ants[i].antX = rand() % 200;
+        ants[i].antY = rand() % 200;
+        ants[i].antDir = rand() % 4;
+        char bufferX[8];
+        char bufferY[8];
+        snprintf(ants[i].cAntX,sizeof(ants[i].cAntX),"%d",ants[i].antX);
+        snprintf(ants[i].cAntY,sizeof(ants[i].cAntY),"%d",ants[i].antY);
     }
+}
+bool RunLauncherFrame(struct nk_context *ctx) {
+    bool startGame = false;
+    UpdateNuklear(ctx);
+    float ratio_two[] = {0.2f, 0.2f, 0.2f, 0.2f, 0.2f};
+    if (nk_begin(ctx, "Settings", nk_rect(50, 50, 800, 800), NK_WINDOW_BORDER | NK_WINDOW_TITLE)) {
+        nk_layout_row(ctx, NK_DYNAMIC, 30, 5, ratio_two);
+        if (nk_button_label(ctx, "START SIM")) {
+            startGame = true;
+        }
+        if (nk_button_label(ctx, "ADD ANT")) {
+            antcount++;
+            if (antcount > 8) antcount = 8;
+        }
+        if (nk_button_label(ctx, "REMOVE ANT")) {
+            antcount--;
+            if (antcount < 1) antcount = 1;
+        }
+        if (nk_button_label(ctx, "ADD COLOUR")) {
+            colourCount++;
+            if (colourCount > 20) colourCount = 20;
+        }
+        if (nk_button_label(ctx, "REMOVE COLOUR")) {
+            colourCount--;
+            if (colourCount < 2) colourCount = 2;
+        }
+        //Add default, random, and maybe symmetry?
+
+        nk_layout_row_dynamic(ctx, 30, 3);
+        for (int i = 0; i < antcount; i++) {
+            char bufferX[25] = "";
+            char bufferY[25] = "";
+            char butterDir[25] = "";
+            sprintf(bufferX, "Ant %d X coordinate", i + 1);
+            sprintf(bufferY, "Ant %d y coordinate", i + 1);
+            sprintf(butterDir, "Ant %d direction", i + 1);
+
+            nk_label(ctx, bufferX, NK_TEXT_LEFT);
+            
+            nk_label(ctx, bufferY, NK_TEXT_LEFT);
+            nk_label(ctx, butterDir, NK_TEXT_LEFT);
+            nk_edit_string(ctx, NK_EDIT_SIMPLE, ants[i].cAntX, &antlen[i][0], 4, nk_filter_decimal);
+            ants[i].antX = atoi(ants[i].cAntX);
+            if(ants[i].antX >= 200) ants[i].antX = 199;
+            if(ants[i].antX < 0) ants[i].antX = 0;
+            nk_edit_string(ctx, NK_EDIT_SIMPLE, ants[i].cAntY, &antlen[i][1], 4, nk_filter_decimal);
+            if(ants[i].antY >= 200) ants[i].antY = 199;
+            if(ants[i].antY < 0) ants[i].antY = 0;
+            nk_combobox(ctx, dir_pointers, 4, &selected_dirs[i], 25, nk_vec2(nk_widget_width(ctx), 200));
+        }
+
+        float widths[] = {15, 60, 15, 60, 15, 60, 50, 100, 200};
+        for (int i = 0; i < colourCount; i++) {
+            Colours[i].r = atoi(Colours[i].cr);
+            if (Colours[i].r > 255) Colours[i].r = 255;
+            if (Colours[i].r < 0)   Colours[i].r = 0;
+
+            Colours[i].g = atoi(Colours[i].cg);
+            if (Colours[i].g > 255) Colours[i].g = 255;
+            if (Colours[i].g < 0)   Colours[i].g = 0;
+
+            Colours[i].b = atoi(Colours[i].cb);
+            if (Colours[i].b > 255) Colours[i].b = 255;
+            if (Colours[i].b < 0)   Colours[i].b = 0;
+
+            nk_layout_row(ctx, NK_STATIC, 30, 9, widths);
+            nk_label(ctx, "R", NK_TEXT_CENTERED);
+            nk_edit_string(ctx, NK_EDIT_SIMPLE, Colours[i].cr, &colourlen[i][0], 4, nk_filter_decimal);
+            nk_label(ctx, "G", NK_TEXT_CENTERED);
+            nk_edit_string(ctx, NK_EDIT_SIMPLE, Colours[i].cg, &colourlen[i][1], 4, nk_filter_decimal);
+            nk_label(ctx, "B", NK_TEXT_CENTERED);
+            nk_edit_string(ctx, NK_EDIT_SIMPLE, Colours[i].cb, &colourlen[i][2], 4, nk_filter_decimal);
+            nk_label(ctx, "RULE", NK_TEXT_CENTERED);
+            nk_combobox(ctx, rule_pointers, 4, &selected_rules[i], 25, nk_vec2(nk_widget_width(ctx), 200));
+            nk_button_color(ctx, nk_rgb(Colours[i].r, Colours[i].g, Colours[i].b));
+        }
+    }
+    nk_end(ctx);
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawNuklear(ctx);
+    EndDrawing();
+
+    return startGame;
+}
+void runSimFrame(RenderTexture2D canvas) {
+    BeginTextureMode(canvas);
+    for (int i = 0; i < antcount; i++) {
+        if (ants[i].antDead) continue;
+        int x = ants[i].antX;
+        int y = ants[i].antY;
+        
+        if (x < 0 || x >= 200 || y < 0 || y >= 200) {
+            ants[i].antDead = true;
+            continue;
+        }
+
+        int currentIndex = grid[y][x];
+        Rule currentRule = Colours[currentIndex].colourRule;
+
+        switch (currentRule) {
+            case R: ants[i].antDir = (ants[i].antDir + 1) % 4; break;
+            case L: ants[i].antDir = (ants[i].antDir + 3) % 4; break;
+            case U: ants[i].antDir = (ants[i].antDir + 2) % 4; break;
+            case C: break;
+        }
+
+
+        grid[y][x] = (currentIndex + 1) % colourCount;
+        tileColour tile = Colours[grid[y][x]];
+        DrawRectangle(x * 5, y * 5, 5, 5, (Color){tile.r, tile.g, tile.b, 255});
+
+        switch (ants[i].antDir) {
+            case UP:    ants[i].antY--; break;
+            case RIGHT: ants[i].antX++; break;
+            case DOWN:  ants[i].antY++; break;
+            case LEFT:  ants[i].antX--; break;
+        }
+    }
+    EndTextureMode();
+
+    BeginDrawing();
+        ClearBackground(DARKGRAY); 
+        DrawTextureRec(canvas.texture, (Rectangle){ 0, 0, (float)canvas.texture.width, (float)-canvas.texture.height }, (Vector2){ 0, 0 }, WHITE);
+    EndDrawing();
 }
 
 int main(void) {
-    CLI();
+    srand(time(NULL));
+    InitColourStruct();
+    InitAntStruct();
+    InitWindow(1000, 1000, "Langton's Ant");
+    SetTargetFPS(100000);
+    const int fontSize = 20;
+    Font font = LoadFontFromNuklear(fontSize);
+    struct nk_context *ctx = InitNuklearEx(font, fontSize);
+
+    while (!WindowShouldClose()) {
+        if (RunLauncherFrame(ctx)) break;
+    }
+
+    for (int i = 0; i < antcount; i++) {
+        ants[i].antX = atoi(ants[i].cAntX);
+        ants[i].antY = atoi(ants[i].cAntY);
+        ants[i].antDir = (Direction)selected_dirs[i];
+        ants[i].antDead = false;
+    }
+    for (int i = 0; i < colourCount; i++) {
+        Colours[i].r = atoi(Colours[i].cr);
+        Colours[i].g = atoi(Colours[i].cg);
+        Colours[i].b = atoi(Colours[i].cb);
+        Colours[i].colourRule = (Rule)selected_rules[i];
+    }
+
+    UnloadNuklear(ctx);
+
+    BeginDrawing();
+    EndDrawing();
+    RenderTexture2D canvas = LoadRenderTexture(1000, 1000);
+    BeginTextureMode(canvas);
+    ClearBackground(RAYWHITE); 
+    EndTextureMode();
+    while (!WindowShouldClose()) {
+        runSimFrame(canvas);
+    }
+
+    CloseWindow();
     return 0;
 }
